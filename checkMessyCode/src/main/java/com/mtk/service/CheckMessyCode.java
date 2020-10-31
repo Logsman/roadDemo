e301db02600a8f70f4a7e337c7b965da159c0146
road <123@qq.com>
2020-10-31 12:44:48 +0800

    commit-test

---
+++ checkMessyCode/src/main/java/com/mtk/service/CheckMessyCode.java
@@ -0,0 +1,13 @@
+package com.mtk.service;
+
+/**
+ * @className CheckMessyCode
+ * @Description j
+ * @author ROAD
+ * @date 2020/10/31 12:06
+ */
+public class CheckMessyCode {
+    public static void main(String[] args) {
+
+    }
+}
